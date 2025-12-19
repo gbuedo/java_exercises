b3527d97f8e02dcfefb5bc29949eeeab5ec5f4ac
Gaston Buedo <gbuedo@gmail.com>
2025-12-19 00:40:10 -0300

    Primer upload de ejercicios realizados con Java

---
+++ Java/holamundo.java
@@ -0,0 +1,6 @@
+public class holamundo {
+    // Método principal
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ Java/presentacion.java
@@ -0,0 +1,7 @@
+public class presentacion {
+    public static void main(String[] args) {
+        System.out.println("Hola, Mi nombre es Gaston");
+        System.out.println("Tengo 25 años");
+        System.out.println("Vivo en Argentina");
+    }
+}
